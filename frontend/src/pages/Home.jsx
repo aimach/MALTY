da6256b7a78243d93cf7db564b97c37e5419add7
Marie-Hermine Schneider <schneider.marieh@gmail.com>
2023-04-06 20:21:40 +0200

    add firstpage with title, button and img

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import Title from "../components/Title";
+import Navbar from "../components/Navbar";
+import beerImg from "../assets/img/bottle-caps.jpg";
+import "../assets/css/home.css";
+
 export default function Home() {
   return (
-    <header className="App-header">
-      <h1>MALTY</h1>
-      <p>Raise the bar !</p>
-    </header>
+    <body>
+      <Navbar />
+      <Title />
+      <div>
+        <img src={beerImg} alt="beer bottle" className="cover-img" />
+      </div>
+
+      {/* <p>https://punkapi.com/documentation/v2</p>
+      <p>https://dribbble.com/shots/20534330-Case-Study-MOVA-Brewery-Website</p> */}
+    </body>
   );
 }
