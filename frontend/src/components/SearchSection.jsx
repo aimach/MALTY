f851af2fdff5be7ed5e1df0d0d8419cf3e307045
Marie-Hermine Schneider <schneider.marieh@gmail.com>
2023-04-08 17:40:56 +0200

    add search by name and results

--- frontend/src/components/SearchSection.jsx
+++ frontend/src/components/SearchSection.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../assets/css/searchSection.css";
 import axios from "axios";
 import BeerCard from "./BeerCard";
