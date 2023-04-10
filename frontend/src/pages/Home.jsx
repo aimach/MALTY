7184e505f0a6d9921631069b3a5b01b242e4121a
Marie-Hermine Schneider <schneider.marieh@gmail.com>
2023-04-10 21:40:43 +0200

    get images name for random display

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import SearchSection from "../components/SearchSection";
 import Title from "../components/Title";
 import Navbar from "../components/Navbar";
-import beerImg from "../assets/img/bottle-caps.jpg";
 import "../assets/css/home.css";
 import SurpriseSection from "../components/SurpriseSection";
+// import beerImg from "../assets/img/beerImg/beer.jpg";
 
 export default function Home() {
+  // get all images name
+  const images = import.meta.glob("../assets/img/beerImg/*");
+  // get object length
+  const max = Object.values(images).length - 1;
+  // get a random number
+  const randomNbr = Math.floor(Math.random() * (max - 0 + 1)) + 0;
+  // get random img name
+  // const beerImg = `src/${Object.values(images)[randomNbr].name}`;
+  const beerImg = Object.values(images)[randomNbr].name;
+
   return (
     <div>
       <Navbar />
