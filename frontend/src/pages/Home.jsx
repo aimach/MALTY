61b2d331928abee01111d6518ec5709be152fb0e
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-04-12 18:34:03 +0200

    change buttons on homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,10 +8,8 @@ export default function Home() {
       <Navbar />
       <Title />
       <div className="cover-image-container">
-        <div className="choice-button">
-          <button type="button">I already know the beer I want</button>
-          <button type="button">Surprise me please</button>
-        </div>
+        <button type="button">Browse all beer</button>
+        <button type="button">Surprise me !</button>
       </div>
       {/* <p>https://punkapi.com/documentation/v2</p>
       <p>https://dribbble.com/shots/20534330-Case-Study-MOVA-Brewery-Website</p> */}
