7e9eb76e64bd9a3a5b716e274c13891079f39814
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-04-12 08:27:22 +0200

    hiding navbar on scroll

---
+++ frontend/src/pages/All.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "@components/Navbar";
+
+function All() {
+  return (
+    <div>
+      <Navbar />
+      All
+    </div>
+  );
+}
+
+export default All;
