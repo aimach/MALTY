73afe214d00a1cb05bfd6bd67d7f64657c1d3ee5
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-04-12 18:20:30 +0200

    last commit before all changes

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Navbar from "@components/Navbar";
+import "../assets/css/about.css";
+import bdlogo from "../assets/img/brewdog-logo.png";
 
 function About() {
   return (
-    <div>
+    <div className="about">
       <Navbar />
-      About
+      <div className="about-section">
+        <img src={bdlogo} alt="brewdog logo" width="100px" />
+        <h1>About Malty and Punk API</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          corporis itaque blanditiis velit. Rerum provident debitis, eveniet
+          amet nobis sed nihil! Numquam iste sapiente ea, modi deserunt
+          accusantium eaque aliquam!
+        </p>
+      </div>
     </div>
   );
 }
